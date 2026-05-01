9ad4ad36995e4069c4f5ab959cf438dd6923d3a5
vimalraj <vimalrajvj1048@gmail.com>
2026-05-02 00:05:10 +0530

    examples added in redis.ts

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -5,6 +5,8 @@ const redis = new Redis({
     token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 });
 
+// example of redis rate limiting in next js api route
+
 // import { NextRequest } from "next/server";
 // import { redis } from "@/lib/redis";
 
@@ -30,4 +32,35 @@ const redis = new Redis({
 
 //   // 5. Your actual API logic
 //   return Response.json({ message: "Success" });
+// }
+
+// example of redis implementation in next js api route
+
+
+//  app/api/latestInterviews/route.ts
+// import { redis } from "@/lib/redis";
+
+// export async function GET() {
+//   const cacheKey = "latest_interviews";
+
+//   // 1. Check cache
+//   const cached = await redis.get(cacheKey);
+
+//   if (cached) {
+//     return Response.json({
+//       source: "cache",
+//       data: cached,
+//     });
+//   }
+
+//   // 2. Fetch from DB
+//   const interviews = await getInterviewsFromDB();
+
+//   // 3. Store in Redis (TTL 60 sec)
+//   await redis.set(cacheKey, interviews, { ex: 60 });
+
+//   return Response.json({
+//     source: "db",
+//     data: interviews,
+//   });
 // }
